5652e88d5204aa24a8fbc09a16f2b28a889e3b8a
VivkyAAAA <dk1006ching@gmail.com>
2022-01-19 17:55:03 +0800

    v0119_5

--- js/menu.js
+++ js/menu.js
@@ -4,6 +4,17 @@ let menuHtml = `
 <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="products.html">商品展示</a></li>
 <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="store.html">購物車</a></li>
 `;
+
+let m = '';
+m += '<li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="index.html">首頁</a></li>';
+m += '<li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="about.html">關於我</a></li>';
+m += '<li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="products.html">商品展示</a></li>';
+m += '<li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="store.html">購物車</a></li>';
+
+
+
+
+
 // 取得 DOM  的節點
 let menu = document.getElementById('menu');
 //menu.textContent =  menuHtml;  //寫入節點  文字內容
@@ -11,19 +22,34 @@ let menu = document.getElementById('menu');
 
 let menuData = [
     {
-        tital:'首頁',
+        title:'首頁',
         url:'index.html'
-    };
+    },
     {
-        tital:'關於我',
+        title:'關於我',
         url:'about.html'
-    };
+    },
     {
-        tital:'商品展示',
+        title:'商品展示',
         url:'products.html'
-    };
+    },
     {
-        tital:'>購物車',
+        title:'購物車',
         url:'store.html'
-    };
-]
+    }
+];
+
+/**
+ menuHtml = menuHtml + `<li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="index.html">Home</a></li>`;
+ row =   {
+        title: '購物車',
+        url: 'store.html'
+    }
+ */
+    menuHtml = '';
+    for(let row of menuData){
+        menuHtml += `<li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="${row.url}">${row.title}</a></li>\n`;
+
+        console.log(menuHtml);
+    }
+    menu.innerHTML = menuHtml;//將 HTML 寫入節點
