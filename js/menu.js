5a66863500d47eaca76557f07bf8be0e17778410
VivkyAAAA <dk1006ching@gmail.com>
2022-01-19 15:56:43 +0800

    v0119_4

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,29 @@
-let menuHtml = '
+let menuHtml = `
 <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="index.html">首頁</a></li>
 <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="about.html">關於我</a></li>
 <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="products.html">商品展示</a></li>
 <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="store.html">購物車</a></li>
-';
+`;
 // 取得 DOM  的節點
 let menu = document.getElementById('menu');
-menu.textContent =  menuHtml;
+//menu.textContent =  menuHtml;  //寫入節點  文字內容
+//menu.innerHTML = menuHtml;  //將 HTML 寫入節點
+
+let menuData = [
+    {
+        tital:'首頁',
+        url:'index.html'
+    };
+    {
+        tital:'關於我',
+        url:'about.html'
+    };
+    {
+        tital:'商品展示',
+        url:'products.html'
+    };
+    {
+        tital:'>購物車',
+        url:'store.html'
+    };
+]
