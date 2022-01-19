23f70d46b2efaa9c00a24f2e716f90c7eb9d43f5
VivkyAAAA <dk1006ching@gmail.com>
2022-01-19 15:29:09 +0800

    v0119_3

--- js/menu.js
+++ js/menu.js
@@ -1,26 +1,9 @@
-/**
- <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="index.html">首頁</a></li>
- <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="about.html">關於我</a></li>
- <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="products.html">商品展示</a></li>
- <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="store.html">購物車</a></li>
- */
-
- let menuData = [
-     {
-         title:'首頁',
-         url:'index.html'
-     },
-     {
-        title:'關於我',
-        url:'about.html'
-    },
-    {
-        title:'商品展示',
-        url:'products.html'
-    },
-    {
-        title:'購物車',
-        url:'store.html'
-    },
-
- ];
+let menuHtml = '
+<li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="index.html">首頁</a></li>
+<li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="about.html">關於我</a></li>
+<li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="products.html">商品展示</a></li>
+<li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="store.html">購物車</a></li>
+';
+// 取得 DOM  的節點
+let menu = document.getElementById('menu');
+menu.textContent =  menuHtml;
